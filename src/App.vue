723095a391c530420edf86e1f5f2ccfddc89f3cf
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2018-11-14 00:59:51 +0800

    first draft of functional tabs using slots

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!--
+      <SlotExample :msg="msg">
+        <h2 slot="lineOne">parent message one to child who shows the message</h2>
+        <h2 slot="lineTwo">parent message Two to child who shows the message</h2>
+      </SlotExample>
+    -->
+    <Tabs></Tabs>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import SlotExample from "./components/SlotExample.vue";
+  import Tabs from "./components/Tabs.vue";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "app",
+    components: {
+      SlotExample,
+      Tabs
+    },
+    data() {
+      return {
+        msg: "messages from parent to child who shows the message"
+      };
+    }
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    /*font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;*/
+  }
 </style>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="Tabs">
+    <!--
+      <h1>{{ msg }}</h1>
+      Child One<br />
+      <slot name="lineOne"></slot> Child Two <br />
+      <slot name="lineTwo"></slot>
+    -->
+    <div class="myButtonDiv">
+      <button
+        v-for="(tab,index) in tabs"
+        :class="{active : currentTab === index }"
+        @click="currentTab = index"
+      >
+        {{ tab }}
+      </button>
+    </div>
+    <div class="tab-content">
+      <div v-show="currentTab === 0"><h1>Tab1 content</h1></div>
+      <div v-show="currentTab === 1"><h1>Tab2 content</h1></div>
+      <div v-show="currentTab === 2"><h1>Tab3 content</h1></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Tabs",
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        currentTab: 0,
+        tabs: ["Funny Dog", "Funny Cat", "Funny ?"]
+      };
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .tabs {
+  }
+  .myButtonDiv {
+    border: 5px solid red;
+
+    padding: 30px;
+    width: 60%;
+    margin: 0 auto;
+  }
+  div.myButtonDiv {
+    border: 5px solid black;
+    display: flex;
+    justify-content: flex-start;
+    padding-top: 100px;
+    padding-bottom: 0;
+
+    /*padding-top: 100px;
+    padding-bottom: 0;
+    padding-right: 200px;*/
+  }
+  button {
+    padding: 10px 30px;
+    margin-right: 5px;
+  }
+  button.active {
+    background-color: lightgreen;
+  }
+  .tab-content {
+    padding: 30px;
+    border: 1px solid #ccc;
+    width: 60%;
+    margin: 0 auto;
+    text-align: center;
+  }
+</style>
